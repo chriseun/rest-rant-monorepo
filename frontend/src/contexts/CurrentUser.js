d1c9de0fe21fc83c94ae748f50b33f9a56394d91
Chris Eun <eunchristopher@gmail.com>
2023-04-26 19:16:40 -0700

    iam signup and login

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
 
 
 export const CurrentUser = createContext()
@@ -6,6 +7,16 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
+    useEffect(() =>{
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:3000/authentication/profile', {
+                credentials: 'include'
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
